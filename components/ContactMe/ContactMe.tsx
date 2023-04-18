f201143241e8eee6f985ec784ed2c47f0ae46540
sargsyansevak <thesevaksargsyan@gmail.com>
2023-04-18 21:26:51 +0400

    first

--- components/ContactMe/ContactMe.tsx
+++ components/ContactMe/ContactMe.tsx
@@ -6,7 +6,7 @@ import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import CallIcon from "@mui/icons-material/Call";
 import CloseIcon from "@mui/icons-material/Close";
-import Image from 'next/image'
+import Image from "next/image";
 const steps = [
   {
     label: "SEND A MESSAGE IN A BOTTLE",
@@ -55,7 +55,7 @@ const ContactMe = () => {
   };
   return (
     <>
-      <div className="contact-me-container ">
+      <div className="contact-me-container">
         <div className="open fixed right-4 top-4 md:right-10">
           <Button
             variant="text"
@@ -67,34 +67,31 @@ const ContactMe = () => {
           </Button>
         </div>
       </div>
-      <div
-        className={`bg-yellow-50 contact-me h-screen fixed right-0 top-0 bottom-0 z-50 ${
-          ownWidth ? "w-80" : "hidden"
-        }`}
+
+      <div 
+       className={`bg-yellow-50 contact-me h-screen fixed right-0 top-0 bottom-0 z-50 ${
+        ownWidth ? "w-70" : "hidden"
+      }`}
       >
-        <Button
-          variant="text"
-          color="success"
-          startIcon={<CloseIcon />}
-          className="text-black border-x-1 border-y-1 bg-inherit  border-x-white border-y-white btn_text pt-4 pl-2"
-          onClick={() => setOwnWidth(false)}
-        >
-          Close
-        </Button>
-
-        <div className="contact-body py-8 absolute">
-          <div className="contact-description flex justify-center items-center flex-col gap-10">
-            <Image src="/main-logo.png" alt="" width={80} height={80} />
-            <h4 className="text-lg text-orange-400">Have you any questions?</h4>
-            <h6>Im at your services</h6>
-          </div>
-          <div className="input-form">
-            <h3 className="text-bold text-blue-500">
-              {steps[activeStep].label}
-            </h3>
-            <div>{steps[activeStep].description}</div>
-          </div>
-
+        <div className="contact_title  h-1/6 pt-4 pl-2">
+          <Button
+            variant="text"
+            color="success"
+            startIcon={<CloseIcon />}
+            className="text-black border-x-1 border-y-1 bg-inherit  border-x-white border-y-white btn_text"
+            onClick={() => setOwnWidth(false)}
+          >
+            Close
+          </Button>
+        </div>
+        <div className="contact-logo  h-1/3 flex flex-col justify-evenly items-center">
+          <Image src="/main-logo.png" alt="" width={80} height={80} />
+          <h4 className="text-lg text-orange-400">Have you any questions?</h4>
+          <h6>Im at your services</h6>
+        </div>
+        <div className="contact-input  h-1/2 flex flex-col justify-center items-center">
+          <h3 className="text-bold text-blue-500">{steps[activeStep].label}</h3>
+          <div>{steps[activeStep].description}</div>
           <MobileStepper
             className="bg-inherit gap-12"
             variant="text"
@@ -132,6 +129,72 @@ const ContactMe = () => {
           />
         </div>
       </div>
+      {/* <div
+        className={`bg-yellow-50 contact-me h-screen fixed right-0 top-0 bottom-0 z-50 ${
+          ownWidth ? "w-80" : "hidden"
+        }`}
+      >
+        <Button
+          variant="text"
+          color="success"
+          startIcon={<CloseIcon />}
+          className="text-black border-x-1 border-y-1 bg-inherit  border-x-white border-y-white btn_text pt-4 pl-2"
+          onClick={() => setOwnWidth(false)}
+        >
+          Close
+        </Button>
+
+        <div className="contact-body py-8 absolute">
+          <div className="contact-description flex justify-center items-center flex-col gap-10">
+            <Image src="/main-logo.png" alt="" width={80} height={80} />
+            <h4 className="text-lg text-orange-400">Have you any questions?</h4>
+            <h6>Im at your services</h6>
+          </div>
+          <div className="input-form">
+            <h3 className="text-bold text-blue-500">
+              {steps[activeStep].label}
+            </h3>
+            <div>{steps[activeStep].description}</div>
+          
+
+          <MobileStepper
+            className="bg-inherit gap-12"
+            variant="text"
+            steps={maxSteps}
+            position="static"
+            activeStep={activeStep}
+            nextButton={
+              <Button
+                size="small"
+                onClick={handleNext}
+                disabled={activeStep === maxSteps - 1}
+              >
+                Next
+                {theme.direction === "rtl" ? (
+                  <KeyboardArrowLeft />
+                ) : (
+                  <KeyboardArrowRight />
+                )}
+              </Button>
+            }
+            backButton={
+              <Button
+                size="small"
+                onClick={handleBack}
+                disabled={activeStep === 0}
+              >
+                {theme.direction === "rtl" ? (
+                  <KeyboardArrowRight />
+                ) : (
+                  <KeyboardArrowLeft />
+                )}
+                Back
+              </Button>
+            }
+          />
+          </div>
+        </div>
+      </div> */}
     </>
   );
 };
