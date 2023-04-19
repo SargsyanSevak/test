4f4c14c0a6fdb971d0a1a8a7a55fead17d6abe39
sargsyansevak <thesevaksargsyan@gmail.com>
2023-04-19 20:40:40 +0400

    changed bg

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-const { config } = require('process')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  test : /\.svg$/,
-  use : [{
-    loader :'@svgr/webpack',
-    options:{
-      icon:true
-    }
-  }]
 }
+
 module.exports = nextConfig
