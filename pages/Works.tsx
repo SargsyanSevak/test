d820f4513ec8c7a214f43f9c69c887c9b950ed49
sargsyansevak <thesevaksargsyan@gmail.com>
2023-04-20 19:52:10 +0400

    portfolio

--- pages/Works.tsx
+++ pages/Works.tsx
@@ -5,33 +5,39 @@ import Button from "@mui/material/Button";
 import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import Head from "next/head";
-import Image from 'next/image'
+import Image from "next/image";
 const portfolio = [
   {
-    name:'Armstroyservice',
-    url:'https://chicago.aiga.org/wp-content/cache/ikit/image_resize/54c90e577c0897cb924e65632404bf0a/8761d09c20ef1c2ec4b2c33ae9a07231/fc5a120561408f41c104ea1d46d08ad6-1600x.jpg'
+    step: 0,
+    name: "Armstroyservice",
+    url: "https://chicago.aiga.org/wp-content/cache/ikit/image_resize/54c90e577c0897cb924e65632404bf0a/8761d09c20ef1c2ec4b2c33ae9a07231/fc5a120561408f41c104ea1d46d08ad6-1600x.jpg",
   },
   {
-    name:'Marvel Information Portal',
-    url:'https://pbs.twimg.com/media/FoeMW74WAAQVMtM.jpg'
+    step: 1,
+    name: "Marvel Information Portal",
+    url: "https://pbs.twimg.com/media/FoeMW74WAAQVMtM.jpg",
   },
   {
-    name:'UTL Load Board',
-    url:'https://chicago.aiga.org/wp-content/cache/ikit/image_resize/54c90e577c0897cb924e65632404bf0a/8761d09c20ef1c2ec4b2c33ae9a07231/fc5a120561408f41c104ea1d46d08ad6-1600x.jpg'
+    step: 2,
+    name: "UTL Load Board",
+    url: "https://chicago.aiga.org/wp-content/cache/ikit/image_resize/54c90e577c0897cb924e65632404bf0a/8761d09c20ef1c2ec4b2c33ae9a07231/fc5a120561408f41c104ea1d46d08ad6-1600x.jpg",
   },
   {
-    name:'Portfolio Website',
-    url:'https://pbs.twimg.com/media/FoeMW74WAAQVMtM.jpg'
+    step: 3,
+    name: "Portfolio Website",
+    url: "https://pbs.twimg.com/media/FoeMW74WAAQVMtM.jpg",
   },
   {
-    name:'Naxshun React Shop',
-    url:'https://chicago.aiga.org/wp-content/cache/ikit/image_resize/54c90e577c0897cb924e65632404bf0a/8761d09c20ef1c2ec4b2c33ae9a07231/fc5a120561408f41c104ea1d46d08ad6-1600x.jpg'
+    step: 4,
+    name: "Naxshun React Shop",
+    url: "https://chicago.aiga.org/wp-content/cache/ikit/image_resize/54c90e577c0897cb924e65632404bf0a/8761d09c20ef1c2ec4b2c33ae9a07231/fc5a120561408f41c104ea1d46d08ad6-1600x.jpg",
   },
   {
-    name:'ToDo',
-    url:'https://pbs.twimg.com/media/FoeMW74WAAQVMtM.jpg'
-  }
-]
+    step: 5,
+    name: "ToDo",
+    url: "https://pbs.twimg.com/media/FoeMW74WAAQVMtM.jpg",
+  },
+];
 export default function DotsMobileStepper() {
   const theme = useTheme();
   const [activeStep, setActiveStep] = React.useState(0);
@@ -51,24 +57,88 @@ export default function DotsMobileStepper() {
         <meta name="description" content="sargsyan" />
       </Head>
 
-      <div className="container bg-red-400 flex justify-around items-center">
-        <div className="stepper h-screen w-52 bg-pink-600 hidden md:block">
-
+      <div className="container flex justify-around items-center">
+        <div className="stepper h-screen w-52  hidden md:flex flex-col justify-center items-center gap-4">
+          <button
+            onClick={handleBack}
+            disabled={activeStep === 0 ? true : false}
+            className={`${activeStep === 0 ? "text-gray-400" : ""}`}
+          >
+            Previous
+          </button>
+          <div className="stepper-col bg-white w-1 h-96">
+            <ul className="transition-both">
+              {portfolio.map((el, i) => (
+                <li
+                  key={i}
+                  className={`${
+                    activeStep == el.step ? "bg-red-800" : "bg-white"
+                  } w-1 h-16  transition-all cursor-pointer`}
+                  onClick={() => setActiveStep(el.step)}
+                ></li>
+              ))}
+            </ul>
+          </div>
+          <button
+            onClick={handleNext}
+            disabled={activeStep === 5 ? true : false}
+            className={`${activeStep === 5 ? "text-gray-400" : ""}`}
+          >
+            Next
+          </button>
         </div>
-        <div className="portfolio w-screen md:w-3/4 h-screen bg-yellow-400 flex flex-col justify-center">
-          <div className="title bg-green-500 w-4/4 h-20 flex justify-center items-center">
-            <p className="text-4xl font-bold tracking-wide">{portfolio[activeStep].name}</p>
+        <div className="portfolio w-screen  md:w-3/4 h-screen  flex flex-col justify-center">
+          <div className="title w-4/4 h-20 flex justify-center items-center">
+            <p className="text-2xl md:text-4xl font-bold tracking-wide">
+              {portfolio[activeStep].name}
+            </p>
           </div>
-          <div className="portfolio-body bg-pink-500 w-4/4 h-96 flex justify-center items-center overflow-hidden">
-            <img src={portfolio[activeStep].url} alt={portfolio[activeStep].name}width={600} height={500} />
+          <div className="portfolio-body w-4/4 h-72 md:h-96 flex justify-center items-center overflow-hidden">
+            <img
+              src={portfolio[activeStep].url}
+              alt={portfolio[activeStep].name}
+              width={600}
+              height={500}
+            />
           </div>
-          <div className="discover-btn bg-purple-500 w-4/4 h-20 flex justify-center items-center">
-          <Button variant="text" className="text-white btn_text">
-            Discover +
-          </Button>
+          <div className="discover-btn  w-4/4 h-20 flex justify-center items-center">
+            <Button variant="text" className="text-white btn_text">
+              Discover +
+            </Button>
           </div>
-          <div className="title bg-green-500 w-4/4 h-20 flex justify-center items-center md:hidden">
-         
+
+          <div className="  w-4/4 h-20 flex justify-center items-center md:hidden">
+            <div className=" h-6 w-52 flex flex-row justify-center items-center">
+              <ul className="transition-both rotate-90">
+                {portfolio.map((el, i) => (
+                  <li
+                    key={i}
+                    className={`${
+                      activeStep == el.step ? "bg-red-800" : "bg-white"
+                    } w-1 h-16  transition-all cursor-pointer`}
+                    onClick={() => setActiveStep(el.step)}
+                  ></li>
+                ))}
+              </ul>
+              {/* <div className="">
+                <button
+                  onClick={handleBack}
+                  disabled={activeStep === 0 ? true : false}
+                  className={`${
+                    activeStep === 0 ? "text-gray-400" : ""
+                  } `}
+                >
+                  Next
+                </button>
+                <button
+                  onClick={handleNext}
+                  disabled={activeStep === 5 ? true : false}
+                  className={`${activeStep === 5 ? "text-gray-400" : ""} `}
+                >
+                  Previous
+                </button>
+              </div> */}
+            </div>
           </div>
         </div>
       </div>
