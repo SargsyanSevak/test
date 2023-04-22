6769741f328ebab94f731f9837b3ddaa64f6ac83
sargsyansevak <thesevaksargsyan@gmail.com>
2023-04-23 01:16:32 +0400

    carusel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,7 +33,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;300;400;700&display=swap" />
-        <link href="https://fonts.googleapis.com/css2?family=Bruno+Ace&display=swap" rel="stylesheet"/>
+       
       </Head>
       <Component {...pageProps} />
     </Layout>
