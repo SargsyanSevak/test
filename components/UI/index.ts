952601ce09883fa88b23b3e6040a965942220486
sargsyansevak <thesevaksargsyan@gmail.com>
2023-04-23 12:40:22 +0400

    some

--- components/UI/index.ts
+++ components/UI/index.ts
@@ -3,6 +3,5 @@ export * from './Tag/Tag';
 export * from './P/P';
 export * from './Htag/Htag';
 export * from './AudioPlayer/AudioPlayer';
-export * from './DiscoverBtn/DiscoverBtn';
 export * from './Input/Input';
 export * from './TextArea/TextArea';
